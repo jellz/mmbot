a26636f5f09b6637fd392ae6cb6581e5c05fb877
Jellz <xdjellz@gmail.com>
2018-06-20 18:55:46 +1000

    throw command

---
+++ commands/Utility/throw.js
@@ -0,0 +1,16 @@
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			guarded: true,
+			description: 'This command will throw an error'
+		});
+	}
+
+	async run(msg) {
+        throw 'Oh no there is an error :(';
+	}
+
+};
