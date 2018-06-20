5c56683f182cc6316134ff27b4a6284eeea59b2e
Jellz <xdjellz@gmail.com>
2018-06-20 19:12:27 +1000

    minimize cat fact command

--- commands/Fun/catfact.js
+++ commands/Fun/catfact.js
@@ -12,9 +12,7 @@ module.exports = class extends Command {
 	async run(msg) {
         const m = await msg.channel.send('🐱 Getting random cat fact...');
         const res = await fetch('https://catfact.ninja/fact');
-        const body = await res.json();
-        if (!res.ok) return m.edit('Something went wrong while querying the API. If this issue persists, please join the support server (%help) and report the bug.');
-        return m.edit('🐱 ' + body.fact);
+        return m.edit('🐱 ' + (await res.json()).fact);
     }
 
 };
