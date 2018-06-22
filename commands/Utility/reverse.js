568aeed6bb12c32b0fb79ec054a51e10a701cb97
Jellz <xdjellz@gmail.com>
2018-06-22 22:55:11 +1000

    reverse command

---
+++ commands/Utility/reverse.js
@@ -0,0 +1,16 @@
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+            description: 'Reverse text',
+            usage: '<query:str> [...]'
+		});
+	}
+
+	async run(msg, [query]) {
+        return msg.channel.send(query.split('').reverse().join(''));
+	}
+
+};
