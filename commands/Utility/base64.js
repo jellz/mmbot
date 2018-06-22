01f5bfde345bacc6832500966efbc96815ee7ac7
Jellz <xdjellz@gmail.com>
2018-06-22 22:12:04 +1000

    base 64 command

---
+++ commands/Utility/base64.js
@@ -0,0 +1,16 @@
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+            description: 'Convert normal text into Base 64 because why not',
+            usage: '<query:str> [...]'
+		});
+	}
+
+	async run(msg, [query]) {
+        return msg.channel.send(Buffer.from(query, 'utf8').toString('base64'));
+	}
+
+};
