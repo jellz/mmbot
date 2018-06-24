c0d0db61d819e22444be3ab98a9b7a24334a7440
Jellz <xdjellz@gmail.com>
2018-06-24 18:33:24 +1000

    fix fix music

--- commands/Music/fixmusic.js
+++ commands/Music/fixmusic.js
@@ -11,8 +11,8 @@ module.exports = class extends Command {
 
 	async run(msg) {
         this.client.musicQueues[msg.guild.id] = [];
-        msg.guild.voiceConnection.dispatcher.end();
         msg.guild.voiceConnection.disconnect();
+        return msg.reply('I have attempted to fix the music system. If the issue persists, please join the support server and tell us.');
 	}
 
 };
